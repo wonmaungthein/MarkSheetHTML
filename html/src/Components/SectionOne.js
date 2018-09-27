97ad56cc5f1dad2f3e0fe9113da2fef047d2e750
Won Maung Thein <wonmaungthein@gmail.com>
2018-09-27 16:57:45 +0100

    Added a new SectionOne component

---
+++ html/src/Components/SectionOne.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class SectionOne extends Component {
+	render() {
+		return (
+			<div>
+				<section className="section-one">
+					<div>
+						<p>
+							Have you seen this <a href="https://www.youtube.com">amazing video</a> on YouTube?
+						</p>
+						<p>
+							Sir <strong>Alex Ferguson</strong> once said about Filipo Inzaghi:{' '}
+							<q> That lad must have been born offside. </q>
+						</p>
+					</div>
+				</section>
+			</div>
+		);
+	}
+}
+
+export default SectionOne;
